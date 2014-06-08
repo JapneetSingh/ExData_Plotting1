0e41372cf9a4dc2552fbbfd9bd57202506ad0c74
JapneetSingh <japneet13@gmail.com>
2014-06-07 22:50:30 -0700

    Files for Course Project 1

--- Plot 1/plot1.r
+++ Plot 1/plot1.r
@@ -14,7 +14,7 @@ data<- data[(data$Date == "2007-02-01")|(data$Date == "2007-02-02"),]
 
 # Step 2-PLotting the data 
 png("plot1.png", width = 480, height = 480)
-plot1<- hist(data$Global_active_power,col="red", xlab="Global Active Power(kilowatts)", main = "Global Active power")
+plot1<- hist(data$Global_active_power,col="red", xlab="Global Active Power(kilowatts)", main = "Global Active Power")
 dev.off()
 
 
